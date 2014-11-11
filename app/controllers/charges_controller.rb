fe621c1a45b14246075e0c07c0accad2536f52bf
brimatteng <btenggren20@gmail.com>
2014-11-11 16:05:05 -0500

    Add a new and Create method in charges controller   - Sets the customer and charge for stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,32 @@
 class ChargesController < ApplicationController
+
+  def new
+    # Renders a charges form
+  end
+
+  def create
+    # Charge amount is in pennies
+    @amount = 500
+
+    # Create a customer using and email and card token
+    # The stripeToken is created for us by Stripe's Checkout
+    customer = Stripe::Customer.create(
+      :email => 'example@stripe.com',
+      :card => params[:stripeToken]
+    )
+
+    # Create a charge using the customer and amount
+    charge = Stripe::Charge.create(
+      :customer => customer.id,
+      :amount => @amount,
+      # Description is optional
+      :description => 'Rails Stripe Customer example',
+      :currency => 'usd'
+    )
+
+    # incase of invalid card or failed charge
+  rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to charges_path
+  end
 end
